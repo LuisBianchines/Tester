bc610634103c80f3f25c36a80940448d9447401a
LuisBianchines <luishdb@hotmail.com>
2021-10-27 21:20:32 -0300

    primeiro-commit

---
+++ features/login.feature
@@ -0,0 +1,32 @@
+#language:pt
+
+Funcionalidade: Login
+    Para que eu possa cadastrar e gerencias minhas tarefas
+    Sendo um Usuario
+    Posso acessar o sistema com meu email e senha previamente cadastrados.
+
+    Cenario: Usuario deve ser autorizado
+
+        Dado que eu acesso a página principal
+        Quando eu faço login com "eu@eu.io" e "12345"
+        Então devo ser autenticado com sucesso
+        E devo ver a seguinte mensagem "Olá, Fernando"
+
+    Cenario: Senha errada
+
+        Dado que eu acesso a página principal
+        Quando eu faço login com "eu@eu.io" e "1234dsf5"
+        Então devo ver a seguinte mensagem "Senha inválida"  
+
+    Cenario: Usuário não existe
+
+        Dado que eu acesso a página principal
+        Quando eu faço login com "eu@eu.net" e "12345"
+        Então devo ver a seguinte mensagem "Usuário não cadastrado."
+
+    Cenario: Email incorreto
+
+        Dado que eu acesso a página principal
+        Quando eu faço login com "eueu.io" e "12345"
+        Então devo ver a seguinte mensagem "Email incorreto ou ausente."
+
---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,17 @@
+
+
+  Dado('que eu acesso a página principal') do                                   
+    pending # Write code here that turns the phrase above into concrete actions 
+  end                                                                           
+                                                                                
+  Quando('eu faço login com {string} e {string}') do |email, senha|          
+    pending # Write code here that turns the phrase above into concrete actions 
+  end                                                                           
+                                                                                
+  Então('devo ser autenticado com sucesso') do                                  
+    pending # Write code here that turns the phrase above into concrete actions 
+  end                                                                           
+                                                                                
+  Então('devo ver a seguinte mensagem {string}') do |mensagem|                    
+    pending # Write code here that turns the phrase above into concrete actions 
+  end                                                                           
